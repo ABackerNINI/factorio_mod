f73524db761dfdf2303090bddf0ed075dbebf76b
ABackerNINI <1105908317@qq.com>
2018-10-23 03:04:35 +0800

    update some configs

--- ab_oreslogisticscenter/config.lua
+++ ab_oreslogisticscenter/config.lua
@@ -8,8 +8,8 @@ function get_config()
         --check all requester chests every nth tick,default = 121
         check_rc_on_nth_tick = 121,
 
-        --capacity of the logistics center per ore,default = 1000
-        lc_capacity = 1000,
+        --capacity of the logistics center per ore,default = 1000000
+        lc_capacity = 1000000, --[1M] 
 
         --stability of collecter chests,default = 0.9
         cc_stability = 0.9,
@@ -17,19 +17,19 @@ function get_config()
         --stability of requester chests,default = 0.9
         rc_stability = 0.9,
 
-        --power consumption of collector chests per distance per item transferring
-        cc_power_consumption = 1000, --J
+        --power consumption of collector chests per distance per item transferring,default = 1000
+        cc_power_consumption = 1000,  --J
 
-        --power consumption of requester chests per distance per item transferring
-        rc_power_consumption = 10,   --J
+        --power consumption of requester chests per distance per item transferring,default = 100
+        rc_power_consumption = 100,   --J
 
-        --logistic slots count of requester chests
-        rc_logistic_slots_count = 2,
+        --logistic slots count of requester chests,default = 5
+        rc_logistic_slots_count = 5,
 
-        --item slot count of logistics center
-        lc_item_slot_count = 200,
+        --item slot count of logistics center,default = 50
+        lc_item_slot_count = 50,
 
-        eei_basic_power_consumption = 1000000, --W[1MW]
+        eei_basic_power_consumption = 1000000, --W [1MW]
 
         eei_input_flow_limit = 10000000, --W [10MW]
         
