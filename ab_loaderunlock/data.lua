03046ad1abbfc31247249997b760f5ef7508ffe9
ABackerNINI <1105908317@qq.com>
2018-12-25 06:31:43 +0800

    remove item.loader.flags.hidden flag

--- ab_loaderunlock/data.lua
+++ ab_loaderunlock/data.lua
@@ -3,11 +3,22 @@
 if data.raw.recipe["loader"] ~= nil then
     data.raw.recipe["loader"].enabled = true
 end
+if data.raw.item["loader"] ~= nil then
+    data.raw.item["loader"].flags = {"goes-to-quickbar"}
+end
+
 -- fast-loader
 if data.raw.recipe["fast-loader"] ~= nil then
     data.raw.recipe["fast-loader"].enabled = true
 end
+if data.raw.item["fast-loader"] ~= nil then
+    data.raw.item["fast-loader"].flags = {"goes-to-quickbar"}
+end
+
 -- express-loader
 if data.raw.recipe["express-loader"] ~= nil then
     data.raw.recipe["express-loader"].enabled = true
+end
+if data.raw.item["express-loader"] ~= nil then
+    data.raw.item["express-loader"].flags = {"goes-to-quickbar"}
 end
