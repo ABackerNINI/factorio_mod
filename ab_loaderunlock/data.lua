d1309ca3ef694c210662f4ae497ca98119b6c1d8
ABackerNINI <1105908317@qq.com>
2019-03-04 22:09:08 +0800

    fix:clear hidden flag

--- ab_loaderunlock/data.lua
+++ ab_loaderunlock/data.lua
@@ -1,24 +1,28 @@
---loader unlock
--- loader
+--loader recipe unlock
+
 if data.raw.recipe["loader"] ~= nil then
     data.raw.recipe["loader"].enabled = true
 end
--- if data.raw.item["loader"] ~= nil then
-    -- data.raw.item["loader"].flags = {"goes-to-quickbar"}
--- end
 
--- fast-loader
 if data.raw.recipe["fast-loader"] ~= nil then
     data.raw.recipe["fast-loader"].enabled = true
 end
--- if data.raw.item["fast-loader"] ~= nil then
---     data.raw.item["fast-loader"].flags = {"goes-to-quickbar"}
--- end
 
--- express-loader
 if data.raw.recipe["express-loader"] ~= nil then
     data.raw.recipe["express-loader"].enabled = true
 end
--- if data.raw.item["express-loader"] ~= nil then
---     data.raw.item["express-loader"].flags = {"goes-to-quickbar"}
--- end
+
+--loader flags clear
+--it has a flag "hidden",clear it or you can't use auto-upgrade
+
+if data.raw.item["loader"] ~= nil then
+    data.raw.item["loader"].flags = {}
+end
+
+if data.raw.item["fast-loader"] ~= nil then
+    data.raw.item["fast-loader"].flags = {}
+end
+
+if data.raw.item["express-loader"] ~= nil then
+    data.raw.item["express-loader"].flags = {}
+end
